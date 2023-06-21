def5e50c26482217cd75a128d3ea1736001d4fba
Alice Wang <alicewang@AlicedeMacBook-Pro.local>
2023-06-22 03:02:24 +0800

    ADD second solution

--- no485.cpp
+++ no485.cpp
@@ -5,31 +5,45 @@ int findMaxConsecutiveOnes(vector<int>& nums)
 {
     int currentCount = 0;
     int maxCount = 0;
-    int previousNum;
+    int previousNum = 0;
     for(int i = 0; i < nums.size(); i++)
     {
         int currentNum = nums[i];
-        if(i == 0)
+        // * solution 1
+        // * check if 1 and compare with previous
+        if(currentNum == 1 && currentNum != previousNum)
         {
-            if(currentNum == 1)
-            {
-                currentCount = 1;
-                if(currentCount > maxCount) { maxCount = currentCount; }
-            }
+            currentCount = 1;
+            if(currentCount > maxCount) { maxCount = currentCount; }
         }
-        else
+        else if(currentNum == 1 && currentNum == previousNum)
         {
-            if(currentNum == 1 && currentNum == previousNum)
-            {
-                currentCount ++;
-                if(currentCount > maxCount) { maxCount = currentCount; }
-            }
-            else if(currentNum == 1 && currentNum != previousNum)
-            {
-                currentCount = 1;
-                if(currentCount > maxCount) { maxCount = currentCount; }
-            }
+            currentCount ++;
+            if(currentCount > maxCount) { maxCount = currentCount; }
         }
+        // * solution 2
+        // * check if first then compare with previous
+        // if(i == 0)
+        // {
+        //     if(currentNum == 1)
+        //     {
+        //         currentCount = 1;
+        //         if(currentCount > maxCount) { maxCount = currentCount; }
+        //     }
+        // }
+        // else
+        // {
+        //     if(currentNum == 1 && currentNum == previousNum)
+        //     {
+        //         currentCount ++;
+        //         if(currentCount > maxCount) { maxCount = currentCount; }
+        //     }
+        //     else if(currentNum == 1 && currentNum != previousNum)
+        //     {
+        //         currentCount = 1;
+        //         if(currentCount > maxCount) { maxCount = currentCount; }
+        //     }
+        // }
         previousNum = nums[i];
     }
     return maxCount;
@@ -40,7 +54,7 @@ int main()
 {
     vector<int> nums = {1,1,0,1,1,1};
     vector<int> nums2 = {1,0,1,1,0,1};
-    vector<int> nums3 = {1};
-    cout << findMaxConsecutiveOnes(nums3);
+    vector<int> nums3 = {0};
+    cout << findMaxConsecutiveOnes(nums2);
     return 0;
 }
