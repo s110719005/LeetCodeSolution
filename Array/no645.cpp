99465a763c16b6d28f5722fa1b14d17ece4e619f
Alice Wang <alicewang@AlicedeMacBook-Pro.local>
2023-06-22 03:43:14 +0800

    ADD solution but not complete yet

--- Array/no645.cpp
+++ Array/no645.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 using namespace std;
-
+/* 
+? You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, 
+? due to some error, one of the numbers in s got duplicated to another number in the set, 
+? which results in repetition of one number and loss of another number.
+? You are given an integer array nums representing the data status of this set after the error.
+? Find the number that occurs twice and the number that is missing and return them in the form of an array.
+*/ 
+vector<int> findErrorNums(vector<int>& nums) 
+{
+    //! duplicate might not be consecutive
+    vector<int> duplicates;
+    int previousNum = 0;
+    int currentNum = 0;
+    int missingNum = 0;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(!(find(nums.begin(), nums.end(), (i+1)) != nums.end())) 
+        {
+            missingNum = (i+1);
+        }
+        currentNum = nums[i];
+        if(currentNum == previousNum)
+        {
+            duplicates.push_back(currentNum);
+        }
+        previousNum = nums[i];
+    } 
+    duplicates.push_back(missingNum);
+    cout << duplicates[0] << "," << duplicates[1];
+    return duplicates;
+}
 int main()
 {
+    vector<int> nums = {1,2,2,4}; //d:2 missing 3
+    vector<int> nums2 = {1,1};// d:1 missing 2
+    vector<int> nums3 = {1,2,3,4,5,5,7};// d:5 missing 6
+    vector<int> nums4 = {6,6,1,4,3,2,7};// d: 6 missing 5
+    vector<int> nums5 = {2,3,2};// d: 2 missing 1
+    findErrorNums(nums4);
     return 0;
 }
